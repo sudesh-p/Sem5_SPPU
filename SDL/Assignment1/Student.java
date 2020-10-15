6af9830dddc0ca08aa31e07f90c1162e209ca6f6
chaudharishreyas <chaudharishreyas@users.noreply.github.com>
2020-10-15 11:05:44 +0530

    Update Student.java

--- SDL/Assignment1/Student.java
+++ SDL/Assignment1/Student.java
@@ -1,45 +1,35 @@
-package Assign1;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
-
-public class Student<T> implements Comparable<Student>{
-
-	
-	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	int roll_no;
-	T name;
-	int marks;
-
-	Student(){
-		this.roll_no = -1;
-		this.marks = -1;
-	}
+package com.company;
+class Student
+{
+	String name;
+	int rolno;
+	String div;
+	double marks;
+	Student(String name, int rolno, String div,double marks)
+	{
+		this.name= name;
+		this.rolno= rolno;
+		this.div=div;
+		this.marks=marks;
 		
-	Student(int roll_no, T name, int marks){
-		this.initialise(roll_no,name,marks);
+	}
+	public void display()
+	{
+		System.out.println("Name: "+name);
+		System.out.println("Rollno: "+rolno);
+		System.out.println("Division: "+div);
+		
+		System.out.println("Marks: "+marks);
+		
+		
+		
+	}
+	public void modify(String name, String div, double marks)
+	{
+		this.name= name;
+		
+		this.div=div;
+		this.marks=marks;
 	}
 	
-	public void initialise(int roll_no, T name, int marks) {
-		this.roll_no = roll_no;
-		this.name = name;
-		this.marks = marks;
 	}
-
-	public void display() {
-		System.out.print("\n"+Integer.toString(this.roll_no)+"\t\t"+this.name+"\t\t\t"+Integer.toString(this.marks));
-	}
-	
-	@Override
-	public int compareTo(Student o) {
-		if(this.marks < o.marks)
-			return 1;
-		else if(this.marks > o.marks)
-			return -1;
-		return 0;
-	}
-
-
-}
